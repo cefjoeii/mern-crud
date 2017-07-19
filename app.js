0ec6091756f87a39f5b9a2b9c8b3b055830a4e64
cefjoeii <cefjoeii@gmail.com>
2017-07-19 23:06:11 +0800

    Use ES6+ arrow functions

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 // Remove this on the production
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -39,7 +39,7 @@ app.use(function(req, res, next) {
 app.use('/api/users', require('./routes/users'));
 
 // Use express's default error handling middleware
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   if (res.headersSent) {
     return next(err);
   }
